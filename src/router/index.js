051d0679b69362a3ede86817eb0e94af8578b04d
Unknown <gibranln@hotmail.com>
2018-11-30 18:17:43 -0600

    progreso con params

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Proyectos from '@/components/Proyectos'
+import Reuniones from '@/components/Reuniones'
+import Reunion from '@/components/Reunion'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'abstract',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Proyectos',
+      component: Proyectos
+    },
+    {
+      path: '/reuniones',
+      name: 'Reuniones',
+      component: Reuniones,
+      props: true
+    },
+    {
+      path: '/reunion',
+      name: 'Reunion',
+      component: Reunion
     }
   ]
 })
